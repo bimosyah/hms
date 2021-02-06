fc162f012d82441790176cca5dae5af57a7f0e27
bimosyah <bimosyahputro88@gmail.com>
2021-02-06 08:39:11 +0700

    change filename

--- application/models/MFasilitas.php
+++ application/models/MFasilitas.php
@@ -3,7 +3,7 @@
 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class MFasilitas extends CI_Model {
+class Mfasilitas extends CI_Model {
 
 	public function insert($data){
 		$query = $this->db->insert('tbl_fasilitas', $data);
